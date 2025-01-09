33df7839732e596fb0fd9c3bfc592d690180386d
ElanZh <elan_zh@163.com>
2025-01-09 12:20:28 +0800

    fix

--- src/main/kotlin/com/example/demo/web/template/MainController.kt
+++ src/main/kotlin/com/example/demo/web/template/MainController.kt
@@ -3,23 +3,25 @@ package com.example.demo.web.template
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("view")
 class MainController {
-    @GetMapping("/")
+    @GetMapping("")
     fun home(model: Model): String {
         model.addAttribute("username", "Elan");
         return "pages/home";
     }
 
-    @GetMapping("/team")
+    @GetMapping("team")
     fun  team( model: Model): String {
         var teamMembers = listOf("Alice", "Bob", "Charlie", "David");
         model.addAttribute("teamMembers", teamMembers);
         return "pages/team";
     }
 
-    @GetMapping("/projects")
+    @GetMapping("projects")
     fun  projects(model: Model): String {
         var projects = listOf("Project 1", "Project 2", "Project 3");
         model.addAttribute("username", "John Doe");
